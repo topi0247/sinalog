2cb2cfec2086bc224891edc9f252e1a8de0babea
topi0247 <karubona.ra247@gmail.com>
2024-09-29 20:57:20 +0900

    feat:Recoil設定

---
+++ front/src/providers/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { ReactNode } from "react";
+import { RecoilRoot } from "recoil";
+
+export default function AppProvider({ children }: Readonly<{ children: ReactNode }>) {
+  return <RecoilRoot>{children}</RecoilRoot>;
+}
