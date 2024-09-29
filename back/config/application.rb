732a6c653e22a038cfdf2242912abe4e1cd50313
topi0247 <karubona.ra247@gmail.com>
2024-09-29 20:30:09 +0900

    fix:railsのバージョンを修正

--- back/config/application.rb
+++ back/config/application.rb
@@ -6,7 +6,7 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    config.load_defaults 7.2
+    config.load_defaults 7.1
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
     config.generators.system_tests = nil
